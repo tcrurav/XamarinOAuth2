40d95d5a90ead2874be5fe1dd35813cb6d600503
tcrurav <tiburcio.cruz@gmail.com>
2019-12-12 10:12:24 +0000

    Working Basic Google Login with OAuth2

---
+++ Droid/CustomUrlSchemeInterceptorActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+namespace OAuthNativeFlow.Droid
+{
+	[Activity(Label = "CustomUrlSchemeInterceptorActivity", NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+	[IntentFilter(
+		new[] { Intent.ActionView },
+		Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+		DataSchemes = new[] { "com.googleusercontent.apps.471013716354-ot0fg0vv74qg8u9vhcnutv266cr752ct" },
+		DataPath = "/oauth2redirect")]
+	public class CustomUrlSchemeInterceptorActivity : Activity
+	{
+        //DataSchemes = new[] { "com.googleusercontent.apps.863494635082-a617p00qkuafejrf3k7hq8s346l0u36h" },
+        protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Convert Android.Net.Url to Uri
+			var uri = new Uri(Intent.Data.ToString());
+            Console.WriteLine("tiburcio: Intent.Data.ToString(): " + Intent.Data.ToString());
+
+			// Load redirectUrl page
+			AuthenticationState.Authenticator.OnPageLoading(uri);
+
+            //3 líneas que tuve que meter yo
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.SingleTop);
+            StartActivity(intent);
+
+            Finish();
+		}
+	}
+}
---
+++ OAuthNativeFlow/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace OAuthNativeFlow
+{
+    public static class Constants
+    {
+        public static string AppName = "XamarinGoogleLogin";
+
+		// OAuth
+		// For Google login, configure at https://console.developers.google.com/
+		public static string iOSClientId = "<insert IOS client ID here>";
+        //Esta es la clave web --> public static string AndroidClientId = "471013716354-klo2p6i3lu4m87ssp78dfancvrc16mtf.apps.googleusercontent.com";
+        public static string AndroidClientId = "471013716354-ot0fg0vv74qg8u9vhcnutv266cr752ct.apps.googleusercontent.com";
+
+        // These values do not need changing
+        public static string Scope = "https://www.googleapis.com/auth/userinfo.email";
+		public static string AuthorizeUrl = "https://accounts.google.com/o/oauth2/auth";
+		public static string AccessTokenUrl = "https://www.googleapis.com/oauth2/v4/token";
+		public static string UserInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
+
+		// Set these to reversed iOS/Android client ids, with :/oauth2redirect appended
+		public static string iOSRedirectUrl = "<insert IOS redirect URL here>:/oauth2redirect";
+        public static string AndroidRedirectUrl = "com.googleusercontent.apps.471013716354-ot0fg0vv74qg8u9vhcnutv266cr752ct:/oauth2redirect";
+    }
+}
